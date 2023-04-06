c0d02f98754bfde4a895eb6fff69d18f20fb5106
Abduqodir0205 <AbduqodirDeveloper@gmail.com>
2023-04-06 09:29:24 +0500

    Add new Repository

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const tbody = document.querySelector("#tbody");
 // const api = `http://35.242.182.188:8080/it-conference-kuva/users/paging?page=${x}&pageSize=${y}`
-let x = 1
-let y = 14
+let x = 1;
+let y = 14;
 
-function fetchReq(){
-
-    
-fetch(
+function fetchReq() {
+  fetch(
     `http://35.242.182.188:8080/it-conference-kuva/users/paging?page=${x}&pageSize=${y}`
   )
     .then((data) => {
       return data.json();
     })
     .then((user) => {
-        console.log(user);
+      // console.log(user);
       let tableTr = user.data.pageData.users;
-      
+
       tableTr.forEach((item, i) => {
         let firstname = item.firstname;
         let lastname = item.lastname;
@@ -25,12 +23,12 @@ fetch(
         let data = item.date;
         let dataDate = data.slice(0, 10);
         let dataTime = data.slice(11, 19);
-        
+
         let dataRes = `${dataDate} ${dataTime}`;
-        
+
         let tr = document.createElement("tr");
-        tr.innerHTML = ' '
-        tr.innerHTML = `
+        tr.innerHTML = " ";
+        (tr.innerHTML = `
         <th scope="row">
         <input type="checkbox">
         </th>
@@ -39,27 +37,32 @@ fetch(
         <td>${lastname}</td>
         <td>${phone}</td>
         <td>${dataRes}</td>
-        <td>${os}</td>`;
-        
-        
-        tbody.appendChild(tr)
+        <td style="display: flex; flex-wrap: wrap; justify-content:center;">${os}</td> `),
+          chill();
+
+        tbody.appendChild(tr);
       });
     });
 }
-fetchReq()
+fetchReq();
 
+function chill() {
+  let child = tbody.childNodes;
+  // console.log(child);
+  for (let index = 0; index < child.length; index++) {
+    const element = child.length + 1;
+    document.getElementById("barchasi").textContent = element;
+    return element;
+  }
+}
 
-let load = document.getElementById('loadA')
-console.log(load);
+let load = document.getElementById("loadA");
+// console.log(load);
 
-load.addEventListener('click', () =>{
-    x +=1
+load.addEventListener("click", () => {
+  x += 1;
 
-    console.log(x,y);
-    fetchReq()
-})
-console.log(x,y);
-
-
-
-
+  console.log(x, y);
+  fetchReq();
+});
+// console.log(x,y);
